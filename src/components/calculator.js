435804dfc43126e36e72baf0899d20b45efafe7c
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-11-30 18:16:09 +0100

    Update the file with events

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,56 +1,70 @@
+/*eslint-disable*/
 import React from 'react';
+import calculate from '../logic/calculate';
 
 class Calculate extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 0,
+      total: 0,
+      next: null,
+      operation: null,
     };
   }
 
-  render() {
-    const { value } = this.state;
+
+  render() { 
+    const { total, next, operation } = this.state;
+    const buttonPress = (e) => {
+      this.setState(calculate(this.state, e.target.innerText));
+    };
+    // const handleEven = (elem) => {
+    //   console.log(elem.keyCode);
+    // };
     return (
       <div className="calculator-container">
         <table>
           <tr className="table-row">
             <th colSpan="4" className="table-header">
-              {value}
+              <p>{total}</p>
+              <p>{operation}</p>
+              <p>{next}</p>
+
             </th>
           </tr>
 
           <tr>
-            <td><p>AC</p></td>
-            <td><p>+/-</p></td>
-            <td><p>%</p></td>
-            <td className="operator"><p>+</p></td>
+            <td><p onClick={buttonPress}>AC</p></td>
+            <td><p onClick={buttonPress}>+/-</p></td>
+            <td><p onClick={buttonPress}>%</p></td>
+            <td className="operator"><p onClick={buttonPress}>+</p></td>
           </tr>
 
           <tr>
-            <td><p>7</p></td>
-            <td><p>8</p></td>
-            <td><p>9</p></td>
-            <td className="operator"><p>*</p></td>
+            <td><p onClick={buttonPress}>7</p></td>
+            <td><p onClick={buttonPress}>8</p></td>
+            <td><p onClick={buttonPress}>9</p></td>
+            <td className="operator"><p onClick={buttonPress}>*</p></td>
           </tr>
 
           <tr>
-            <td><p>4</p></td>
-            <td><p>5</p></td>
-            <td><p>6</p></td>
-            <td className="operator"><p>-</p></td>
+            <td><p onClick={buttonPress}>4</p></td>
+            <td><p onClick={buttonPress}>5</p></td>
+            <td><p onClick={buttonPress}>6</p></td>
+            <td className="operator"><p onClick={buttonPress}>-</p></td>
           </tr>
 
           <tr>
-            <td><p>1</p></td>
-            <td><p>2</p></td>
-            <td><p>3</p></td>
-            <td className="operator"><p>+</p></td>
+            <td><p onClick={buttonPress}>1</p></td>
+            <td><p onClick={buttonPress}>2</p></td>
+            <td><p onClick={buttonPress}>3</p></td>
+            <td className="operator"><p onClick={buttonPress}>+</p></td>
           </tr>
 
           <tr>
-            <td colSpan="2"><p>0</p></td>
-            <td><p>.</p></td>
-            <td className="operator"><p>=</p></td>
+            <td colSpan="2"><p onClick={buttonPress}>0</p></td>
+            <td><p  onClick={buttonPress}>.</p></td>
+            <td className="operator"><p onClick={buttonPress}>=</p></td>
           </tr>
         </table>
       </div>
