9510388d241d82f9f95bb3d0c56545b4ea6c6116
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-12-06 23:33:26 +0100

    Create home.js

---
+++ src/pages/home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main>
+    <h2 className="welcome">Welcome to our page</h2>
+    <div className="main-box">
+      <p className="sub-box">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Laborum maxime a ex quaerat nesciunt nostrum labore blanditiis voluptatum,
+        repellendus sed, odio sit eveniet natus harum nisi
+        voluptatem amet. Consequatur, consectetur.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Laborum maxime a ex quaerat nesciunt
+        nostrum labore blanditiis voluptatum, repellendus sed, odio sit eveniet
+        natus harum nisi voluptatem amet. Consequatur, consectetur.
+      </p>
+      <p className="lores">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Laborum maxime a ex quaerat nesciunt nostrum
+        labore blanditiis voluptatum, repellendus sed,
+        odio sit eveniet natus harum nisi voluptatem amet.
+        Consequatur, consectetur.Lorem ipsum dolor sit amet
+        consectetur adipisicing elit.
+        Laborum maxime a ex quaerat nesciunt nostrum
+        labore blanditiis voluptatum, repellendus sed,
+        odio sit eveniet natus harum nisi voluptatem amet.
+        Consequatur, consectetur.
+      </p>
+    </div>
+  </main>
+  );
+}
+
+export default Home;
