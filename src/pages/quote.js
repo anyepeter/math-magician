fd91421944eef61cc45c816a4e8b325a874d569b
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-12-07 07:27:37 +0100

    fix linter error

--- src/pages/quote.js
+++ src/pages/quote.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navigation from './navbar';
+import '../Cssmodules/quote.css';
 
 const Quotes = () => (
   <>
     <div>
       <Navigation />
     </div>
-    <div>
+    <div className="text-container">
       <p>
         Mathematics is not about numbers, equations, computations or algorithms;
         it is about understanding - William Paul Thurston.
